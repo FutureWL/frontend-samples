2f441cfd601068216fbb8a564bd1fe5d5bac7409
FutureWL <624263934@qq.com>
2019-08-22 09:20:48 +0800

    update

--- 03-UI/01-AntDesign-ZORRO/quick-start/src/app/pages/button/button.module.ts
+++ 03-UI/01-AntDesign-ZORRO/quick-start/src/app/pages/button/button.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {NzButtonModule} from 'ng-zorro-antd/button';
 import {ButtonComponent} from "./button.component";
 import {ButtonRoutingModule} from "./button-routing.module";
 
 @NgModule({
-  imports: [CommonModule,ButtonRoutingModule],
+  imports: [CommonModule, ButtonRoutingModule, NzButtonModule],
   declarations: [ButtonComponent],
   exports: [ButtonComponent]
 })
